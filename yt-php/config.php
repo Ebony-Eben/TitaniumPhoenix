cf291cc9a2b8bf88d3bfb87174571c7dcfda0b6d
Daniel Baerwalde <dan@stupus.com>
2018-11-20 12:22:23 -0500

    youtube

---
+++ yt-php/config.php
@@ -0,0 +1,9 @@
+<?php
+define('ROOT_PART', Root_part());
+define('APIKEY', '83hrONo5AzJZh9wEUkVYlxzsDySIpy7gDO4NJn3vv5rYJYTuhzGLC5A3x0NfteGd');//这里改成您的api key
+define('GJ_CODE', 'US');//国家代码
+define('SITE_NAME', 'YouTube');//网站名字
+define('TITLENAME', 'YouTube');//网站名字
+define('EN2DEKEY', 'M4mENWBf822TSHz0riEpY463jhdGkMA9');//一个随机字符串，加解密KEY
+define('EMAIL', 'danbaerwalde@gmail.com');//邮箱
+?>
